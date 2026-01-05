79aa1e5a1d8751adc56b659aa178967515593c37
Om Mittal <om040806mittal@gmail.com>
2026-01-05 22:32:08 +0530

    Jungle Part 2

---
+++ Assets/Scripts/Game/Jungle/DelayLoader.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class DelayedHierarchyToggle : MonoBehaviour
+{
+    public static DelayedHierarchyToggle Instance;
+
+    [SerializeField] private float delay = 7f;
+    [SerializeField] private GameObject objectToDisable;
+    [SerializeField] private GameObject objectToEnable;
+
+    private float timer;
+    private bool running;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    void OnEnable()
+    {
+        ResetTimer();
+    }
+
+    void Update()
+    {
+        if (!running) return;
+
+        timer += Time.unscaledDeltaTime;
+
+        if (timer >= delay)
+        {
+            running = false;
+
+            if (objectToDisable != null)
+                objectToDisable.SetActive(false);
+
+            if (objectToEnable != null)
+                objectToEnable.SetActive(true);
+        }
+    }
+
+    public void ResetTimer()
+    {
+        timer = 0f;
+        running = true;
+    }
+}
---
+++ Assets/Scripts/Game/Jungle/LoopTP.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TeleportTrigger2D : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 teleportPosition =
+        new Vector3(-20.2800007f, 0.180000007f, 0f);
+
+    [SerializeField] private string playerTag = "Player";
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(playerTag)) return;
+
+        other.transform.position = teleportPosition;
+
+        // GUARANTEED RESET
+        if (DelayedHierarchyToggle.Instance != null)
+            DelayedHierarchyToggle.Instance.ResetTimer();
+    }
+}
